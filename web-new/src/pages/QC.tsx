d6a36c13990bd497bee3b25fe7141405ecd8e26f
Harrisaint <hgmartin1116@gmail.com>
2025-09-20 13:05:38 -0400

    Add file extensions to import paths for Vite build compatibility

--- web-new/src/pages/QC.tsx
+++ web-new/src/pages/QC.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Typography } from '@mui/material'
-import { Api } from '@/api/client'
+import { Api } from '@/api/client.ts'
 
 const QC: React.FC = () => {
   const [qc, setQc] = React.useState<any>(null)
